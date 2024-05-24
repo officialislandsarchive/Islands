f8c7c9c0724974903596cb548b3dafcc89bfc3dc
officialislandsarchive <officialislandsarchive@gmail.com>
2024-05-24 18:44:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let modal = document.getElementById('myModal');
 let searchButton = document.getElementById("search");
 let searchEntry = document.getElementById("searchInput");
 
-const serverUrl = 'https://5f5eb461-0a6a-4785-b438-d1291ff9ea1e-00-e5o1geiav5mz.riker.replit.dev/';
+const serverUrl = 'https://5f5eb461-0a6a-4785-b438-d1291ff9ea1e-00-e5o1geiav5mz.riker.replit.dev:3001/';
 
 let items = {
     "crops": [
