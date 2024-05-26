4863013940ac8bd6d8ee02c108f4a9e3d4a186ac
officialislandsarchive <officialislandsarchive@gmail.com>
2024-05-26 16:36:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,120 +6,121 @@ const serverUrl = 'https://5f5eb461-0a6a-4785-b438-d1291ff9ea1e-00-e5o1geiav5mz.
 
 let items = {
     "crops": [
-        { name: "Wheat", value: "10" },
-        { name: "Tomato", value: "15" },
-        { name: "Carrot", value: "20" },
+        { name: "Wheat", value: "7" },
+        { name: "Tomato", value: "10" },
+        { name: "Carrot", value: "29" },
         { name: "Berry", value: "25" },
-        { name: "Onion", value: "30" },
-        { name: "Red Flowers", value: "35" },
-        { name: "Yellow Flowers", value: "40" },
-        { name: "Blue Flowers", value: "45" },
-        { name: "Tall Grass", value: "50" }
+        { name: "Onion", value: "33" },
+        { name: "Red Flowers", value: "1750" },
+        { name: "Yellow Flowers", value: "1750" },
+        { name: "Blue Flowers", value: "1750" },
+        { name: "Tall Grass", value: "125" }
     ],
 
     "blocks": [
-        { name: "Stone Brick", value: "10" },
-        { name: "Wood", value: "15" },
-        { name: "Wood Plank", value: "20" },
-        { name: "Pine Wood", value: "25" },
-        { name: "Pine Wood Plank", value: "30" },
-        { name: "Grass Block", value: "35" },
-        { name: "Glass Pane", value: "40" },
-        { name: "Glass Pane (Colored)", value: "45" },
-        { name: "Clay", value: "50" },
-        { name: "Clay (Colored)", value: "55" },
-        { name: "Red Rug", value: "60" },
-        { name: "Wood Stairs", value: "65" },
-        { name: "Pine Stairs", value: "70" },
-        { name: "Stone Stairs", value: "75" },
-        { name: "Brick Stairs", value: "80" },
-        { name: "Wood Fence", value: "85" },
-        { name: "Pine Fence", value: "90" },
-        { name: "Ladder", value: "95" },
-        { name: "Pine Door", value: "100" }
+        { name: "Stone", value: "3" },
+        { name: "Stone Brick", value: "50" },
+        { name: "Wood", value: "50" },
+        { name: "Wood Plank", value: "27.5" },
+        { name: "Pine Wood", value: "100" },
+        { name: "Pine Wood Plank", value: "50" },
+        { name: "Grass Block", value: "2" },
+        { name: "Glass Pane", value: "22" },
+        { name: "Glass Pane (Colored)", value: "300" },
+        { name: "Clay", value: "75" },
+        { name: "Clay (Colored)", value: "650" },
+        { name: "Red Rug", value: "50" },
+        { name: "Wood Stairs", value: "25" },
+        { name: "Pine Stairs", value: "40" },
+        { name: "Stone Stairs", value: "38" },
+        { name: "Brick Stairs", value: "60" },
+        { name: "Wood Fence", value: "50" },
+        { name: "Pine Fence", value: "250" },
+        { name: "Ladder", value: "75" },
+        { name: "Pine Door", value: "650" }
     ],
 
     "totems": [
-        { name: "Stone Totem", value: "10" },
-        { name: "Coal Totem", value: "15" },
-        { name: "Iron Totem", value: "20" },
-        { name: "Clay Totem", value: "25" },
-        { name: "Wheat Totem", value: "30" },
-        { name: "Tomato Totem", value: "35" },
-        { name: "Carrot Totem", value: "40" },
-        { name: "Onion Totem", value: "45" }
+        { name: "Stone Totem", value: "1500" },
+        { name: "Coal Totem", value: "2000" },
+        { name: "Iron Totem", value: "3000" },
+        { name: "Clay Totem", value: "7500" },
+        { name: "Wheat Totem", value: "1750" },
+        { name: "Tomato Totem", value: "3500" },
+        { name: "Carrot Totem", value: "4500" },
+        { name: "Onion Totem", value: "6500" }
     ],
 
     "industrial": [
-        { name: "Medium Chest", value: "10" },
-        { name: "Steel Rod", value: "15" },
-        { name: "Conveyor Belt", value: "20" },
-        { name: "Left Conveyor", value: "25" },
-        { name: "Right Conveyor", value: "30" },
-        { name: "Industrial Chest", value: "35" },
-        { name: "Industrial Oven", value: "40" },
-        { name: "Industrial Sawmill", value: "45" },
-        { name: "Industrial Smelter", value: "50" }
+        { name: "Medium Chest", value: "10000" },
+        { name: "Steel Rod", value: "2175" },
+        { name: "Conveyor Belt", value: "2750" },
+        { name: "Left Conveyor", value: "3150" },
+        { name: "Right Conveyor", value: "3150" },
+        { name: "Industrial Chest", value: "12500" },
+        { name: "Industrial Oven", value: "30000" },
+        { name: "Industrial Sawmill", value: "50000" },
+        { name: "Industrial Smelter", value: "400000" }
     ],
 
     "stations": [
-        { name: "Workbench Tier 1", value: "10" },
-        { name: "Workbench Tier 2", value: "15" },
-        { name: "Anvil", value: "20" },
-        { name: "Sawmill", value: "25" },
-        { name: "Stonecutter", value: "30" },
-        { name: "Coloring Station", value: "35" },
-        { name: "Campfire", value: "40" },
-        { name: "Cooking Table", value: "45" },
-        { name: "Floral Bench", value: "50" },
-        { name: "Small Furnace", value: "55" },
-        { name: "Small Chest", value: "60" }
+        { name: "Workbench Tier 1", value: "500" },
+        { name: "Workbench Tier 2", value: "2250" },
+        { name: "Anvil", value: "1000" },
+        { name: "Sawmill", value: "1250" },
+        { name: "Stonecutter", value: "2000" },
+        { name: "Coloring Station", value: "2175" },
+        { name: "Campfire", value: "350" },
+        { name: "Cooking Table", value: "1750" },
+        { name: "Floral Bench", value: "10000" },
+        { name: "Small Furnace", value: "1500" },
+        { name: "Small Chest", value: "1500" }
     ],
 
     "cooking": [
-        { name: "Dough", value: "10" },
-        { name: "Berry Dough", value: "15" },
-        { name: "Bread", value: "20" },
-        { name: "Berry Pie", value: "25" }
+        { name: "Dough", value: "75" },
+        { name: "Berry Dough", value: "300" },
+        { name: "Bread", value: "150" },
+        { name: "Berry Pie", value: "750" }
     ],
 
     "seeds": [
-        { name: "Wheat Seeds", value: "10" },
-        { name: "Tomato Seeds", value: "15" },
-        { name: "Carrot Seeds", value: "20" },
-        { name: "Berry Seeds", value: "25" },
-        { name: "Onion Seeds", value: "30" },
-        { name: "Red Flower Seeds", value: "35" },
-        { name: "Yellow Flower Seeds", value: "40" },
-        { name: "Blue Flower Seeds", value: "45" },
-        { name: "Sapling", value: "50" },
-        { name: "Pine Sapling", value: "55" }
+        { name: "Wheat Seeds", value: "75" },
+        { name: "Tomato Seeds", value: "100" },
+        { name: "Carrot Seeds", value: "275" },
+        { name: "Berry Seeds", value: "7250" },
+        { name: "Onion Seeds", value: "1250" },
+        { name: "Red Flower Seeds", value: "3000" },
+        { name: "Yellow Flower Seeds", value: "3000" },
+        { name: "Blue Flower Seeds", value: "3000" },
+        { name: "Sapling", value: "25" },
+        { name: "Pine Sapling", value: "250" }
     ],
 
     "slimeIsland": [
-        { name: "Green Slime Balls", value: "10" },
-        { name: "Blue Slime Balls", value: "15" },
-        { name: "Pink Slime Balls", value: "20" },
-        { name: "Green Gear", value: "25" },
-        { name: "Blue Gear", value: "30" },
-        { name: "Pink Gear", value: "35" }
+        { name: "Green Slime Balls", value: "15" },
+        { name: "Blue Slime Balls", value: "125" },
+        { name: "Pink Slime Balls", value: "375" },
+        { name: "Green Gear", value: "3250" },
+        { name: "Blue Gear", value: "8500" },
+        { name: "Pink Gear", value: "85000" }
     ],
 
     "tools": [
-        { name: "Wooden Axe", value: "10" },
-        { name: "Wooden Pickaxe", value: "15" },
-        { name: "Wooden Sword", value: "20" },
-        { name: "Plow", value: "25" },
-        { name: "Stone Axe", value: "30" },
-        { name: "Stone Pickaxe", value: "35" },
-        { name: "Iron Axe", value: "40" },
-        { name: "Iron Pickaxe", value: "45" }
+        { name: "Wooden Axe", value: "300" },
+        { name: "Wooden Pickaxe", value: "300" },
+        { name: "Wooden Sword", value: "500" },
+        { name: "Plow", value: "250" },
+        { name: "Stone Axe", value: "750" },
+        { name: "Stone Pickaxe", value: "750" },
+        { name: "Iron Axe", value: "2250" },
+        { name: "Iron Pickaxe", value: "2250" }
     ],
 
     "ore": [
-        { name: "Coal", value: "10" },
-        { name: "Iron Ore", value: "15" },
-        { name: "Iron", value: "20" }
+        { name: "Coal", value: "25" },
+        { name: "Iron Ore", value: "12" },
+        { name: "Iron", value: "75" }
     ],
 }
 
