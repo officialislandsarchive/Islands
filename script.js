4970e5d760f782fae903e65973810c61bedb0d68
officialislandsarchive <officialislandsarchive@gmail.com>
2024-05-27 17:31:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,131 +1,91 @@
 const modal = document.getElementById('myModal');
-const span = document.getElementsByClassName('close')[0];
 const modalTitle = document.getElementById('modalTitle');
 const modalItems = document.getElementById('modalItems');
 const searchButton = document.getElementById("search");
 const searchEntry = document.getElementById("searchInput");
-
 const serverUrl = 'https://5f5eb461-0a6a-4785-b438-d1291ff9ea1e-00-e5o1geiav5mz.riker.replit.dev:3001/';
+const creditsData = [
+            {
+                name: "dawginator4000",
+                surname: "",
+                role: "Game Developer",
+                description: "The one who makes the magic happen!",
+                image: "https://cdn.discordapp.com/attachments/1239379916257230869/1241459480902893568/noFilter.png?ex=6654d2bd&is=6653813d&hm=7bfe898697c99da6f8c200e62343b164c4985b7a62ef180fad4d19462f7006bd&",
+                robloxProfile: "https://www.roblox.com/users/5847760687/profile"
+            },
+            {
+                name: "PartlyScientific",
+                surname: "",
+                role: "Community Server Founder",
+                description: "The one who keeps the community together!",
+                image: "https://cdn.discordapp.com/attachments/1239379916257230869/1244761242845974589/rIdK1DX.png?ex=665649be&is=6654f83e&hm=d03383a7029f6f30479fc28493793371b633221eb5b659cdd516e840d8c86dac&",
+                robloxProfile: "https://www.roblox.com/users/1647767202/profile"
+            },
+            {
+                name: "riskyworld",
+                surname: "",
+                role: "Community Server Founder",
+                description: "The one who leads the community!",
+                image: "https://cdn.discordapp.com/attachments/1239379916257230869/1241459686461542470/noFilter.png?ex=6654d2ee&is=6653816e&hm=0619b98e5dc10fa6cd1420ee2802ca7a377bf26bb1aa67c084226f0804847e27&",
+                robloxProfile: "https://www.roblox.com/users/137462357/profile"
+            },
+            {
+                name: "Xx_Gamer462",
+                surname: "",
+                role: "Website Manager/Developer",
+                description: "The one who manages everything!",
+                image: "https://cdn.discordapp.com/attachments/1239379916257230869/1241459862811054222/noFilter.png?ex=6654d318&is=66538198&hm=42616a4e58ce752027cbd83803ffd3db443216cf581d77e7c83a1d92f7413c71&",
+                robloxProfile: "https://www.roblox.com/users/3823651589/profile"
+            },
+            {
+                name: "jopkljokez2",
+                surname: "",
+                role: "Website Developer",
+                description: "The one who adds the magic touch!",
+                image: "https://cdn.discordapp.com/attachments/1091908728992837704/1244474341123559454/noFilter.png?ex=66553e8c&is=6653ed0c&hm=7b373d3915f106afe38e2806416774fa3ab9e19af75ab3a03d6a59bbde04f623&",
+                robloxProfile: "https://www.roblox.com/users/1044649449/profile"
+            },];
 
-let items = {
-   "crops": [
-        { name: "Wheat", value: "7" },
-        { name: "Tomato", value: "10" },
-        { name: "Carrot", value: "29" },
-        { name: "Berry", value: "25" },
-        { name: "Onion", value: "33" },
-        { name: "Red Flowers", value: "1750" },
-        { name: "Yellow Flowers", value: "1750" },
-        { name: "Blue Flowers", value: "1750" },
-        { name: "Tall Grass", value: "125" }
-    ],
+document.getElementById("creditsBtn").addEventListener("click", showCredits);
 
-    "blocks": [
-        { name: "Stone", value: "3" },
-        { name: "Stone Brick", value: "50" },
-        { name: "Wood", value: "50" },
-        { name: "Wood Plank", value: "27.5" },
-        { name: "Pine Wood", value: "100" },
-        { name: "Pine Wood Plank", value: "50" },
-        { name: "Grass Block", value: "2" },
-        { name: "Glass Pane", value: "22" },
-        { name: "Glass Pane (Colored)", value: "300" },
-        { name: "Clay", value: "75" },
-        { name: "Clay (Colored)", value: "650" },
-        { name: "Red Rug", value: "50" },
-        { name: "Wood Stairs", value: "25" },
-        { name: "Pine Stairs", value: "40" },
-        { name: "Stone Stairs", value: "38" },
-        { name: "Brick Stairs", value: "60" },
-        { name: "Wood Fence", value: "50" },
-        { name: "Pine Fence", value: "250" },
-        { name: "Ladder", value: "75" },
-        { name: "Pine Door", value: "650" }
-    ],
+function showCredits() {
+    modal.style.display = "block";
+    modalTitle.textContent = "Credits";
+    modalItems.innerHTML = creditsData.map(person => `
+        <div class="item-box credit-item" data-url="${person.robloxProfile}">
+            <img src="${person.image}" alt="${person.name}" class="credit-image">
+            <div class="credit-details">
+                <div class="credit-name">${person.name} ${person.surname}</div>
+                <div class="credit-role">${person.role}</div>
+                <div class="credit-description">${person.description}</div>
+            </div>
+        </div>
+    `).join('');
 
-    "totems": [
-        { name: "Stone Totem", value: "1500" },
-        { name: "Coal Totem", value: "2000" },
-        { name: "Iron Totem", value: "3000" },
-        { name: "Clay Totem", value: "7500" },
-        { name: "Wheat Totem", value: "1750" },
-        { name: "Tomato Totem", value: "3500" },
-        { name: "Carrot Totem", value: "4500" },
-        { name: "Onion Totem", value: "6500" }
-    ],
+    document.querySelectorAll('.credit-item').forEach(item => {
+        item.addEventListener('click', function() {
+            window.open(this.getAttribute('data-url'), '_blank');
+        });
+    });
+}
 
-    "industrial": [
-        { name: "Medium Chest", value: "10000" },
-        { name: "Steel Rod", value: "2175" },
-        { name: "Conveyor Belt", value: "2750" },
-        { name: "Left Conveyor", value: "3150" },
-        { name: "Right Conveyor", value: "3150" },
-        { name: "Industrial Chest", value: "12500" },
-        { name: "Industrial Oven", value: "30000" },
-        { name: "Industrial Sawmill", value: "50000" },
-        { name: "Industrial Smelter", value: "400000" }
-    ],
+function closeModal() {
+    modal.style.display = "none";
+}
 
-    "stations": [
-        { name: "Workbench Tier 1", value: "500" },
-        { name: "Workbench Tier 2", value: "2250" },
-        { name: "Anvil", value: "1000" },
-        { name: "Sawmill", value: "1250" },
-        { name: "Stonecutter", value: "2000" },
-        { name: "Coloring Station", value: "2175" },
-        { name: "Campfire", value: "350" },
-        { name: "Cooking Table", value: "1750" },
-        { name: "Floral Bench", value: "10000" },
-        { name: "Small Furnace", value: "1500" },
-        { name: "Small Chest", value: "1500" }
-    ],
+function closeSpecialMessageModal() {
+    const modal = document.getElementById("specialMessageModal");
+    modal.style.display = "none";
+}
 
-    "cooking": [
-        { name: "Dough", value: "75" },
-        { name: "Berry Dough", value: "300" },
-        { name: "Bread", value: "150" },
-        { name: "Berry Pie", value: "750" }
-    ],
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("search").click();
+    }
+});
 
-    "seeds": [
-        { name: "Wheat Seeds", value: "75" },
-        { name: "Tomato Seeds", value: "100" },
-        { name: "Carrot Seeds", value: "275" },
-        { name: "Berry Seeds", value: "7250" },
-        { name: "Onion Seeds", value: "1250" },
-        { name: "Red Flower Seeds", value: "3000" },
-        { name: "Yellow Flower Seeds", value: "3000" },
-        { name: "Blue Flower Seeds", value: "3000" },
-        { name: "Sapling", value: "25" },
-        { name: "Pine Sapling", value: "250" }
-    ],
-
-    "slimeIsland": [
-        { name: "Green Slime Balls", value: "15" },
-        { name: "Blue Slime Balls", value: "125" },
-        { name: "Pink Slime Balls", value: "375" },
-        { name: "Green Gear", value: "3250" },
-        { name: "Blue Gear", value: "8500" },
-        { name: "Pink Gear", value: "85000" }
-    ],
-
-    "tools": [
-        { name: "Wooden Axe", value: "300" },
-        { name: "Wooden Pickaxe", value: "300" },
-        { name: "Wooden Sword", value: "500" },
-        { name: "Plow", value: "250" },
-        { name: "Stone Axe", value: "750" },
-        { name: "Stone Pickaxe", value: "750" },
-        { name: "Iron Axe", value: "2250" },
-        { name: "Iron Pickaxe", value: "2250" }
-    ],
-
-    "ore": [
-        { name: "Coal", value: "25" },
-        { name: "Iron Ore", value: "12" },
-        { name: "Iron", value: "75" }
-    ],
-};
+searchButton.onclick = searchAndWin;
 
 function handleSearch() {
     const keyword = searchEntry.value.toLowerCase();
@@ -197,24 +157,6 @@ if (!visited) {
     sessionStorage.setItem('visited', true);
 }
 
-function showCredits() {
-    modal.style.display = "block";
-    modalTitle.textContent = "Credits";
-    modalItems.innerHTML = creditsData.map(person => `
-        <a href="${getRobloxProfileUrl(person.name)}" target="_blank" class="credit-link">
-            <div class="item-box">
-                <img src="${person.image}" alt="${person.name}" class="credit-image">
-                <div class="credit-details">
-                    <div class="credit-name">${person.name} ${person.surname}</div>
-                    <div class="credit-role">${person.role}</div>
-                    <div class="credit-description">${person.description}</div>
-                </div>
-            </div>
-        </a>
-    `).join('');
-}
-
-
 function searchAndWin() {
     var searchTerm = searchInput.value.trim().toLowerCase();
     if (searchTerm === "memorial") {
@@ -240,18 +182,3 @@ function claimCoins() {
 }
 
 searchButton.onclick = searchAndWin;
-
-function sendDiscordMessage(message) {
-    var webhookURL = "https://discord.com/api/webhooks/1244492846698860634/HQKLhGISvncGm4IuNNUiWbP8GZVsm6n2FPEl8Wuu_bqj-uu-7T8Mx_ys-djKFvPBGmH1";
-    var data = JSON.stringify({ content: message });
-
-    fetch(webhookURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: data,
-    })
-    .then(response => console.log('Message sent to Discord'))
-    .catch(error => console.error('Error sending message to Discord:', error));
-}
